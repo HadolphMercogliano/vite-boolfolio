a97916185f18ac9663a6c0255d2a537fe47df5c6
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-04-24 20:43:26 +0200

    import bs + axios req + print response

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import axios from "axios";
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  data() {
+    return {
+      title: "Hello",
+      projects: [],
+    };
+  },
+  methods: {
+    fetchProjects() {
+      axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
+        console.log(response);
+        this.projects = response.data;
+      });
+    },
+  },
+  created() {
+    this.fetchProjects();
+  },
+
+  components: { AppHeader },
+};
+</script>
+
+<template>
+  <header>
+    <AppHeader />
+  </header>
+  <h1>{{ title }}</h1>
+  <ul>
+    <li v-for="project in projects">Titolo: {{ project.title }}</li>
+  </ul>
+</template>
+
+<style lang="sass" scoped></style>
---
+++ src/components/ProjectList.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<template>
+  <ul>
+    <li v-for="project in projects">{{ project.title }}</li>
+  </ul>
+</template>
+
+<style lang="sass" scoped></style>
